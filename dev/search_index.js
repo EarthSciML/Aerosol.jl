35a1995e437a0a4006e540f42508a9138c3baa62
Documenter.jl <documenter@juliadocs.github.io>
2022-04-28 20:13:19 +0000

    build based on 4b93a6f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Aerosol","category":"page"},{"location":"#Aerosol","page":"Home","title":"Aerosol","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Aerosol.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Aerosol]","category":"page"}]
+}
