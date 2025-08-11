69d861190aa2dd934799cabadc1cb011f9637760
Christopher Tessum <ctessum@gmail.com>
2025-08-11 12:48:21 -0500

    Update to MTKv10 and change tests to testitems

--- src/Aerosol.jl
+++ src/Aerosol.jl
@@ -4,12 +4,13 @@ using Reexport
 using DynamicQuantities
 using ModelingToolkit
 using ModelingToolkit: t, D
+using EarthSciMLBase
 
 @register_unit ppb 1
 
 include("VBS.jl")
 include("elemental_carbon.jl")
-include("isorropia/isorropia.jl")
-@reexport using .ISORROPIA
+#include("isorropia/isorropia.jl")
+#@reexport using .ISORROPIA
 
 end
--- src/elemental_carbon.jl
+++ src/elemental_carbon.jl
@@ -11,32 +11,33 @@ any dynamics.
 Default density is from: https://doi.org/10.5194/acp-23-4327-2023
 """
 function ElementalCarbon(; name = :ElementalCarbon)
-    params = @parameters(ρ=1.1,
-        [unit = u"kg/m^3", description = "Particle density"],
-        d_p=0.8e-6,
-        [unit = u"m", description = "Particle diameter"],
-        T=298.15,
-        [unit = u"K", description = "Temperature"],
-        P=101325,
-        [unit = u"Pa", description = "Pressure"],)
-    vars = @variables(EC(t)=1.0,
-        [unit = u"ppb", description = "Elemental Carbon concentration"],
+    params = @parameters begin
+        ρ=1.1, [unit = u"kg/m^3", description = "Particle density"]
+        d_p=0.8e-6,[unit = u"m", description = "Particle diameter"]
+        T=298.15, [unit = u"K", description = "Temperature"]
+        P=101325, [unit = u"Pa", description = "Pressure"]
+    end
+    vars = @variables begin
+        EC(t)=1.0, [unit = u"ppb", description = "Elemental Carbon concentration"]
         v_placeholder(t),
         [
             unit = u"kg/m^2*K*Pa",
             description = "Placeholder to prevent parameters from being removed"
-        ],)
-    @constants zero=0 [unit = ModelingToolkit.get_unit(EC / t), description = "Zero change"]
+        ]
+    end
+    consts = @constants begin
+        zero=0, [unit = ModelingToolkit.get_unit(EC / t), description = "Zero change"]
+    end
     eqs = [
         D(EC) ~ zero,
         v_placeholder ~ ρ * d_p * T * P * 1e-20 # Meaningless equation to prevent parameters from being removed.
     ]
-    ODESystem(
+    System(
         eqs,
         t,
         vars,
-        params;
+       [params; consts];
         name = name,
-        metadata = Dict(:coupletype => ElementalCarbonCoupler)
+        metadata = Dict(CoupleType => ElementalCarbonCoupler)
     )
 end
